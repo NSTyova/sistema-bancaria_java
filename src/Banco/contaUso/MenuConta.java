d39303600ad905889ae50bae1d099c46bd21a9dc
NSTyova <salvadortyova@gmail.com>
2023-01-08 22:12:02 +0100

    adcionando a lista

--- src/Banco/contaUso/MenuConta.java
+++ src/Banco/contaUso/MenuConta.java
@@ -4,6 +4,8 @@ import Banco.Conta;
 import Banco.clientes.Cliente;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuConta {
     public static void main(String[] args) {
@@ -16,6 +18,11 @@ public class MenuConta {
         conta.setClientes(clientes);
         conta.levantar(1245L, 50.0, LocalDate.of(2022,10,10));
         System.out.println(conta.dados(conta));
+        
+        List<ContaCorrente> contas = new ArrayList<ContaCorrente>();
+        
+        contas.add(conta);
+        System.out.println(contas.size());
 
     }
 }
