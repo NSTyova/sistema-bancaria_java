605ac676d3f91f1b40080b5a1eb6529ba988d6d4
NSTyova <salvadortyova@gmail.com>
2023-02-14 12:28:16 +0100

    balance account

---
+++ src/arquivos/MenuOption.java
@@ -0,0 +1,17 @@
+package arquivos;
+
+public enum MenuOption {
+
+	// declara o conteúdo do tipo enum
+	ZERO_BALANCE(1),
+	CREDIT_BALANCE(2),
+	DEBIT_BALANCE(3),
+	END(4);
+	
+	private final int value; // opção atual de menu
+	
+	// construtor
+	private MenuOption(int value) {
+		this.value = value;
+	}
+}
